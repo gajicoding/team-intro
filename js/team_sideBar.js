2a3cebe1af28ad4d4108f4e6c6f1b2a0e104267e
huouvcti <huouvcti@gamil.com>
2025-04-08 11:47:28 +0900

    소개 정보 merge

--- js/team_sideBar.js
+++ js/team_sideBar.js
@@ -2,8 +2,11 @@
 
 let data = [
     {
-        img: "https://blog.kakaocdn.net/dn/tEMUl/btrDc6957nj/NwJoDw0EOapJNDSNRNZK8K/img.jpg",
-        name: "홍길동"
+        img: "https://sitem.ssgcdn.com/87/70/47/item/1000026477087_i1_750.jpg",
+        name: "김나경",
+        age: 25,
+        mbti: "INTP",
+        hobby: "수영, 퍼즐"
     },
 
     {
@@ -13,22 +16,22 @@ let data = [
 
     {
         img: "https://blog.kakaocdn.net/dn/tEMUl/btrDc6957nj/NwJoDw0EOapJNDSNRNZK8K/img.jpg",
-        name: "홍길동"
+        name: "김길동"
     },
 
     {
         img: "https://blog.kakaocdn.net/dn/tEMUl/btrDc6957nj/NwJoDw0EOapJNDSNRNZK8K/img.jpg",
-        name: "홍길동"
+        name: "이길동"
     },
 
     {
         img: "https://blog.kakaocdn.net/dn/tEMUl/btrDc6957nj/NwJoDw0EOapJNDSNRNZK8K/img.jpg",
-        name: "홍길동"
+        name: "나길동"
     },
 
     {
         img: "https://blog.kakaocdn.net/dn/tEMUl/btrDc6957nj/NwJoDw0EOapJNDSNRNZK8K/img.jpg",
-        name: "홍길동"
+        name: "가길동"
     },
 ];
 
@@ -54,6 +57,11 @@ $(document).ready(function () {
     
         let i = $(this).index();
         console.log(data[i]);
+
+        $('#info_name').text(data[i].name);
+        $('#info_age').text(data[i].age);
+        $('#info_mbti').text(data[i].mbti);
+        $('#info_hobby').text(data[i].hobby);
     });
 
     // 기본값
